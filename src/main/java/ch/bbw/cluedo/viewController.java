4bf3987747684347419c4523355e0ba70ca15b86
Kirari04 <lev@saminskij.ch>
2022-08-31 10:57:39 +0200

    added update view

--- src/main/java/ch/bbw/cluedo/viewController.java
+++ src/main/java/ch/bbw/cluedo/viewController.java
@@ -2,11 +2,17 @@ package ch.bbw.cluedo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class viewController {
     @GetMapping("/cluedo")
-    public String index(){
+    public String showView(){
+        return "CluedoView";
+    }
+
+    @PostMapping("/cluedo")
+    public String updateView(){
         return "CluedoView";
     }
 }
