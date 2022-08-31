15e98afcb949f7cba32d2eebc35fafd5b6957d15
Kirari04 <lev@saminskij.ch>
2022-08-31 11:01:54 +0200

    renamed Index view

--- src/main/java/ch/bbw/cluedo/homeController.java
+++ src/main/java/ch/bbw/cluedo/homeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class homeController {
     @GetMapping("/")
     public String index(){
-            return  "index";
+            return "Index";
     }
 }
