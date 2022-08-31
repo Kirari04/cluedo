97ae1f0cbaf4934f7e55f7eee3251d6e02002602
Kirari04 <lev@saminskij.ch>
2022-08-31 10:47:55 +0200

    Created Weapon model

---
+++ src/main/java/ch/bbw/cluedo/model/Weapon.java
@@ -0,0 +1,24 @@
+package ch.bbw.cluedo.model;
+
+public class Weapon {
+    private String name;
+
+    public Weapon(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
