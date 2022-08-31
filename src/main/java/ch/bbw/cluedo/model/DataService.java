cafe4e366309d398d7c0584668dcdab582c87901
Kirari04 <lev@saminskij.ch>
2022-08-31 10:48:34 +0200

    Created DataService model

---
+++ src/main/java/ch/bbw/cluedo/model/DataService.java
@@ -0,0 +1,10 @@
+package ch.bbw.cluedo.model;
+
+import java.util.ArrayList;
+
+public class DataService {
+    private ArrayList<Person> persons;
+    private ArrayList<Weapon> weapons;
+    private ArrayList<Room> rooms;
+    private ArrayList<String> history;
+}
