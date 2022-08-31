507b12e49a4af62d83b0efef13b50261746403aa
Kirari <kontakt@senpai.one>
2022-08-31 09:59:18 +0200

    initial commit

---
+++ src/main/java/ch/bbw/cluedo/homeController.java
@@ -0,0 +1,12 @@
+package ch.bbw.cluedo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+    @GetMapping
+    public String show(){
+            return  "index";
+    }
+}
