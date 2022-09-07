3227abd358a613b01b75a7b3eed9d09de2790a0d
Kirari04 <lev@saminskij.ch>
2022-09-07 10:35:30 +0200

    created cluedo model list

---
+++ src/main/java/ch/bbw/cluedo/ViewController.java
@@ -0,0 +1,30 @@
+package ch.bbw.cluedo;
+
+import ch.bbw.cluedo.model.DataService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ViewController {
+    private final DataService service = new DataService();
+
+    @GetMapping("/cluedo")
+    public String showView(){
+        return "CluedoView";
+    }
+
+    @PostMapping("/cluedo")
+    public String updateView(){
+        return "CluedoView";
+    }
+
+    @GetMapping("/cluedo/list")
+    public String showListView(Model model){
+        model.addAttribute("persons", service.getPersons());
+        model.addAttribute("weapons", service.getWeapons());
+        model.addAttribute("rooms", service.getRooms());
+        return "CluedoListView";
+    }
+}
--- src/main/java/ch/bbw/cluedo/model/DataService.java
+++ src/main/java/ch/bbw/cluedo/model/DataService.java
@@ -1,10 +1,56 @@
 package ch.bbw.cluedo.model;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 
+@Service
 public class DataService {
-    private ArrayList<Person> persons;
-    private ArrayList<Weapon> weapons;
-    private ArrayList<Room> rooms;
+    private ArrayList<Person> persons = new ArrayList<Person>();
+    private ArrayList<Weapon> weapons = new ArrayList<Weapon>();
+    private ArrayList<Room> rooms = new ArrayList<Room>();
     private ArrayList<String> history;
+
+    public DataService() {
+        this.persons.add(new Person("John"));
+        this.persons.add(new Person("Bingus"));
+
+        this.weapons.add(new Weapon("Ak47"));
+        this.weapons.add(new Weapon("Attack Helicopter"));
+
+        this.rooms.add(new Room("Secret Room 1"));
+        this.rooms.add(new Room("Secret Room 2"));
+    }
+
+    public ArrayList<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(ArrayList<Person> persons) {
+        this.persons = persons;
+    }
+
+    public ArrayList<Weapon> getWeapons() {
+        return weapons;
+    }
+
+    public void setWeapons(ArrayList<Weapon> weapons) {
+        this.weapons = weapons;
+    }
+
+    public ArrayList<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(ArrayList<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    public ArrayList<String> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<String> history) {
+        this.history = history;
+    }
 }
